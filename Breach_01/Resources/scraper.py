de5465bcefdadb8321855d5a8acb12551791a8bd
Laurent Brusa <wrmultitudes@gmail.com>
2026-03-08 14:47:31 +0100

    scraaing

---
+++ Breach_01/Resources/scraper.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "http://localhost:8080/.hidden/"
+
+# the common messages to ignore
+TROLL_MESSAGES = [
+    "Non ce n'est toujours pas bon ...",
+    "Tu veux de l'aide ? Moi aussi !",
+    "Demande à ton voisin du dessous",
+    "Demande à ton voisin de gauche",
+    "Toujours pas tu vas craquer non ?",
+    "Demande à ton voisin de droite",
+    "Demande à ton voisin du dessus",
+
+]
+
+visited = set()
+
+
+def crawl(url) -> None:
+    if url in visited:
+        return
+    visited.add(url)
+
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        for link in soup.find_all("a"):
+            href = link.get("href")
+
+            # skip parent directory links
+            if href == "../":
+                continue
+
+            # build url
+            full_url = url + href
+            if href == "README":
+                content = requests.get(full_url).text.strip()
+                if content not in TROLL_MESSAGES:
+                    print(f"Content: {content}\n")
+                else:
+                    print(".", end="", flush=True)
+            elif href.endswith("/"):
+                crawl(full_url)
+
+    except Exception as e:
+        print(f"Error accessing {url}: {e}")
+
+
+if __name__ == "__main__":
+    crawl(BASE_URL)
